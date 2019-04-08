f1f0ccaa8e27328886ec5682c5ee1ed9d02e68c9
Emil Terman <terman.emil@gmail.com>
2019-04-08 12:51:51 +0300

    finished lab 4 - mail_read

--- Lab4/mail_read.py
+++ Lab4/mail_read.py
@@ -1,5 +1,40 @@
 import sys
 import poplib
+import email
+
+
+def get_mails_body(msg):
+    body = ""
+
+    if msg.is_multipart():
+        for part in msg.walk():
+            ctype = part.get_content_type()
+            cdispo = str(part.get('Content-Disposition'))
+
+            # skip any text/plain (txt) attachments
+            if ctype == 'text/plain' and 'attachment' not in cdispo:
+                body = part.get_payload(decode=True)  # decode
+                break
+    else:
+        body = msg.get_payload(decode=True)
+
+    return body.decode('utf-8')
+
+
+def print_mail_headers(msg):
+    for kv in msg._headers:
+        print('%s: %s' % (kv[0], kv[1]))
+
+
+def pretty_print_email(msg):
+    print("Subject: %s" % msg['Subject'])
+    print("From: %s" % msg['From'])
+    print("To: %s" % msg['To'])
+    print("Body: %s" % get_mails_body(msg))
+
+    print()
+    print("Headers: ----------------------------------------------------------")
+    print_mail_headers(msg)
 
 
 if __name__ == '__main__':
@@ -17,7 +52,10 @@ if __name__ == '__main__':
     id, size = str.split(mail.decode('utf-8'))
     resp, body, octets = server.retr(id)
 
-    for line in body:
-        print(line.decode('utf-8'))
-
     server.quit()
+
+    raw_email = b"\n".join(body)
+    parsed_email = email.message_from_bytes(raw_email)
+
+    pretty_print_email(parsed_email)
+
